13c950ffeae59c183b589e67dcaaebd30650a35a
Ville Takanen <ville.takanen@foodie.fm>
2019-07-31 22:47:24 +0300

    login dialog in place

--- src/App.vue
+++ src/App.vue
@@ -52,14 +52,37 @@
         <span class="font-weight-light">{{$t("wikititle")}}</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon>
+      <v-btn icon @click="dialog = !dialog">
         <v-icon>mdi-login</v-icon>
       </v-btn>
     </v-app-bar>
 
+     <v-dialog
+      v-model="dialog"
+      max-width="800"
+     >
+     <v-card color="pink darken-1" dark>
+      <v-card-title class="pink darken-3">Login</v-card-title>
+      <v-card-text>
+      <v-card-actions>
+              <v-btn>
+                  <v-icon>mdi-google</v-icon>
+              </v-btn>
+              <v-btn>
+                  <v-icon>mdi-facebook</v-icon>
+              </v-btn>
+              <v-btn>
+                  <v-icon>mdi-github-circle</v-icon>
+              </v-btn>
+      </v-card-actions>
+      </v-card-text>
+     </v-card>
+     </v-dialog>
+
     <v-content>
       <router-view></router-view>
     </v-content>
+
   </v-app>
 </template>
 
@@ -82,7 +105,13 @@ export default {
     }
   },
   data: () => ({
-    drawer: false
+    drawer: false,
+    dialog: false
   })
 }
 </script>
+<style>
+.opaque{
+  background: white;
+}
+</style>
