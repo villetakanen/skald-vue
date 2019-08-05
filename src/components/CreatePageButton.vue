348ac4cb83237f79cad2eb43015fd9cc5ea1f337
Ville Takanen <ville.takanen@foodie.fm>
2019-08-05 08:57:24 +0300

    Create site dialog added, needs polish, but is usable

--- src/components/CreatePageButton.vue
+++ src/components/CreatePageButton.vue
@@ -8,34 +8,54 @@
     <v-dialog
       v-model="dialog"
       max-width="800"
-     >
-        <v-card>
-            <v-card-title>Create</v-card-title>
-            <v-card-text>
-              <v-tabs>
-                <v-tab>Site</v-tab>
+      >
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{$t('cpb-title')}}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container
+          grid-list-md
+          >
+            <v-layout
+              wrap
+              >
+              <v-tabs xs12 md12>
+                <v-tab>{{$t('cpb-tab-site')}}</v-tab>
+                <v-tab>Page</v-tab>
               </v-tabs>
-              <v-flex>
-                <p>{{$t('cpb-info')}}</p>
-              </v-flex>
               <v-form v-if="site" @submit.prevent="savePage">
-                <v-flex>
+                <v-flex xs12 md12>
+                  <p>{{$t('cpb-info')}}</p>
+                </v-flex>
+                <v-flex xs12 md6>
                   <v-text-field
                     :label="$t('cpb-site-name-label')"
                     :placeholder="$t('cpb-site-name-placeholder')"
-                    filled
                     @input="updateName"
                     ></v-text-field>
+                </v-flex>
+                <v-flex xs1 md1>
+                  <v-switch v-model="customURLtoggle"></v-switch>
+                </v-flex>
+                <v-flex xs15 md5>
                   <v-text-field
                     :label="$t('cpb-site-url-label')"
                     filled
+                    :readonly="!customURLtoggle"
+                    :value="siteURLproposal"
                     ></v-text-field>
-                  <p>{{siteURL}}, {{siteURLproposal}}</p>
-                </v-flex>
+              </v-flex>
               </v-form>
-            </v-card-text>
-     </v-card>
-     </v-dialog>
+        </v-layout>
+      </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="create()">{{$t('create')}}</v-btn>
+          <v-btn text color="secondary" @click="cancel()">{{$t('cancel')}}</v-btn>  
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
@@ -45,15 +65,30 @@ export default {
     site: true,
     siteURL: null,
     siteURLproposal: null,
-    siteName: ''
+    siteName: '',
+    customURLtoggle: false
   }),
+  /* computed: {
+    customURLtoggle: {
+      get () {
+        return this.customURLtoggle_t
+      },
+      set (e) {
+        this.customURLtoggle_t = e
+        if (!this.customURLtoggle_t) this.siteURL = null
+      }
+    }
+  }, */
   methods: {
     updateName (e) {
       this.siteName = e
-      if (this.siteURL === null) {
-        this.siteURLproposal = this.siteName.split(' ').join('_')
-      } else {
-        this.siteURLproposal = null
+      if (!this.customURLtoggle) {
+        var re = new RegExp('[\\W]', 'g')
+        var s = e.replace(re, '-')
+        while (s.includes('--')) {
+          s = s.split('--').join('-')
+        }
+        this.siteURLproposal = s.toLowerCase()
       }
     }
   }
