b05990e5c20c67ddc99646eb1116631e51520b08
Ville Takanen <ville.takanen@foodie.fm>
2019-08-07 22:38:29 +0300

    editor layout updated

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,50 +1,53 @@
 <template>
-<div>
   <v-container fluid grid-list-md>
     <v-layout>
-        <v-flex xs12 md6>
-          <v-card>
-            <v-toolbar dark>
-              <v-toolbar-title>{{$t('ep-editor-title')}}</v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" @click="updatePage">{{$t("save")}}</v-btn>
-            </v-toolbar>
-          </v-card>
-        </v-flex>
+      <v-flex xs12>
+        <v-card>
+          <v-toolbar dark >
+            <v-toolbar-title>{{$t('ep-editor-title')}}</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon
+              @click="preview=!preview"
+              v-if="preview"><v-icon>mdi-eye</v-icon></v-btn>
+            <v-btn icon
+              @click="preview=!preview"
+               v-if="!preview"><v-icon>mdi-eye-off</v-icon></v-btn>
+
+            <v-btn color="primary" @click="updatePage">{{$t("save")}}</v-btn>
+          </v-toolbar>
+          <v-card-text>
+            <v-container fluid>
+              <v-layout>
+                <v-flex>
+                  <v-form  @submit.prevent="savePage">
+                    <v-text-field
+                      v-model="pageName"
+                      filled></v-text-field>
+                    <v-textarea
+                      auto-grow
+                      filled
+                      v-model="pageContent"></v-textarea>
+                  </v-form>
+                </v-flex>
+                <v-flex v-if="preview" xs12 md6>
+                  <v-card>
+                    <v-card-title>{{pageName}}</v-card-title>
+                    <v-card-text>
+                  <Markdown
+                    :page="{ content: pageContent }"
+                    :theme="Skald"/>
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
-<v-form  @submit.prevent="savePage">
-    <v-container
-        fluid
-      >
-        <v-layout wrap justify-end>
-            <v-flex xs12 md6>
-                <v-textarea
-                    solo
-                    name="content-editor"
-                    auto-grow
-                    :value="page"
-                    @input="updateContent"
-                    ></v-textarea>
-             </v-flex>
-             <v-flex xs12 md6>
-               <v-card>
-                 <v-card-text>
-                  <Markdown
-                    :page="preview"
-                    :theme="selectedTheme"/>
-                 </v-card-text>
-               </v-card>
-             </v-flex>
-        </v-layout>
-        <v-layout>
-             <v-flex xs1 align-self-end>
-                 <v-btn right color="primary" @click="savePage(pageid)">Save</v-btn>
-             </v-flex>
-        </v-layout>
-    </v-container>
-    </v-form>
-    </div>
 </template>
 
 <script>
@@ -53,8 +56,27 @@ import Markdown from '../components/Markdown'
 export default {
   props: ['siteid', 'pageid'],
   created () {
-    this.getPage(this.siteid, this.pageid)
+    this.$store.subscribe((mutation, state) => {
+      // console.log(mutation.type)
+      switch (mutation.type) {
+        case 'binder/setPage':
+          console.log('binder/setPage')
+          this.pageContent = state.binder.page.content
+          this.pageName = state.binder.page.name
+          break
+      }
+    })
+    // Get the page from the binder
+    this.$store.dispatch('binder/openPage', { siteid: this.siteid, pageid: this.pageid })
   },
+  data: () => ({
+    preview: true,
+    pageContent: null,
+    pageName: null
+  }),
+  mounted () {
+  },
+  // up
   methods: {
     getPage (siteid, pageid) {
       // var name = pageid || 'index'
