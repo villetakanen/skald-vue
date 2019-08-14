e6540cb943e7238ca445b361d980290bebcdca2e
Ville Takanen <ville.takanen@foodie.fm>
2019-08-14 11:29:39 +0300

    7.2.0 Attachments work!

--- src/components/ViewAttachment.vue
+++ src/components/ViewAttachment.vue
@@ -1,22 +1,35 @@
 <template>
-    <div>
-        <img style="max-width:100%" v-if="!loading" :src="url"/>
-        <p v-if="loading"><i>{{message}}</i>
-          &nbsp;<v-btn @click="uploader" color="primary" text>upload it</v-btn>
-        </p>
-    </div>
+  <div>
+    <img style="max-width:100%" v-if="!loading" :src="url"/>
+    <p v-if="loading"><i>{{message}}</i>
+      &nbsp;<v-btn @click="dialog=!dialog" color="primary" text>upload it</v-btn>
+    </p>
+    <v-dialog
+      v-model="dialog"
+      max-width="800"
+      >
+      <CardFileUpload
+        :path="path"
+        v-on:uploaded="refresh"/>
+    </v-dialog>
+  </div>
 </template>
 <script>
 import firebase from 'firebase'
+import CardFileUpload from './CardFileUpload'
 
 export default {
   props: [
     'path'
   ],
+  components: {
+    CardFileUpload
+  },
   data: () => ({
     loading: true,
     url: '',
-    message: 'Loading...'
+    message: 'Loading...',
+    dialog: false
   }),
   created () {
     // Get a reference to the storage service, which is used to create references in your storage bucket
@@ -31,6 +44,22 @@ export default {
       this.message = this.path + ' not found.'
       console.log(err.message)
     })
+  },
+  methods: {
+    refresh () {
+      const storage = firebase.storage()
+
+      var pathRef = storage.ref(this.path)
+
+      pathRef.getDownloadURL().then((url) => {
+        this.url = url
+        this.loading = false
+      }).catch((err) => {
+        this.message = this.path + ' not found.'
+        console.log(err.message)
+      })
+      this.dialog = false
+    }
   }
 }
 </script>
